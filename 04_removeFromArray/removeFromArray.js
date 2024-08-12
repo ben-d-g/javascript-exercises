d7c646ff20feeb60aa6c6a9e06e62f0d546bc00a
Ben <ben.d.godfrey2002@gmail.com>
2024-08-12 20:56:18 +0100

    Completed task 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    //start with an empty array, add all items which are not in args
+    let answer = [];
+    for (const item of array){
+        if (!(args.includes(item))){
+            answer.push(item)
+        }
+    }
+    return answer;
 };
 
 // Do not edit below this line
