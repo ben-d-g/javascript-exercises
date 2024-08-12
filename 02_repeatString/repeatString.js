3d4b18b6254f8eb0894cb29c1c929cc6621d6ded
Ben <ben.d.godfrey2002@gmail.com>
2024-08-12 20:40:26 +0100

    Completed task 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeats) {
+    if (repeats < 0){
+        return "ERROR";
+    }
+    
+    const temp = string;
+    let answer = "";
+    for (let i = 0; i < repeats; i++){
+        answer = answer + temp;
+    }
+    return answer;
 };
 
 // Do not edit below this line
