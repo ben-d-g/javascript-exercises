eefb6f6a4fe0eaf84921ba83c15ecb79bcea5c0f
Ben <ben.d.godfrey2002@gmail.com>
2024-08-12 21:35:46 +0100

    Completed task 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(...args) {
+    for (const item of args){
+        if (!(Number.isInteger(item))){
+            return "ERROR";
+        }
+        if (item < 0){
+            return "ERROR";
+        }
+    }
+    let answer = 0;
+    for (let i = Math.min(...args); i <= Math.max(...args); i++){
+        answer += i;
+    }
+    return answer;
 };
 
 // Do not edit below this line
