e5fa647e9ccb05a93ddbe016c5b088cc684de0d1
Ben <ben.d.godfrey2002@gmail.com>
2024-08-14 12:18:04 +0100

    Completed task 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (word) {
 
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    
+    const cleanedString = word
+    .toLowerCase()
+    .split('')
+    .filter((character) => alphanumerical.includes(character))
+    .join('');
+
+    for (let i = 0; i <= Math.floor(cleanedString.length / 2); i++){
+        if (cleanedString[i].toLowerCase() != cleanedString[cleanedString.length - (1 + i)]){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
