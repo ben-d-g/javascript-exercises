6a665ffb44284a01849c26ed96216c7a31c1959d
Ben <ben.d.godfrey2002@gmail.com>
2024-08-14 12:27:40 +0100

    Completed task 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0){
+        return "OOPS";
+    }
+    else{
+        if (n === 0 || n === 1){
+            return n;
+        }
+        else if(n === "0"){
+            return 0;
+        }
+        else{
+            let fibs = [0, 1]
+            while (fibs.length <= n){
+                fibs.push(fibs[fibs.length - 1] + fibs[fibs.length - 2]);
+            }
+            return fibs[fibs.length - 1]
+        }
+    }
 };
 
 // Do not edit below this line
