11c241fed6474024382ee67fb5001ee09f8dac41
Ben <ben.d.godfrey2002@gmail.com>
2024-08-14 12:48:38 +0100

    Completed task 12]

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.forEach(person => {
+        if (!person.yearOfDeath){
+            person.yearOfDeath = new Date().getFullYear();
+        }
+    });
+    let peopleSorted = people.sort((a,b) => {
+        if ((a.yearOfDeath - a.yearOfBirth) < (b.yearOfDeath - b.yearOfBirth)){
+            return 1;
+        }
+        else{
+            return -1;
+        }
+    });
+    return peopleSorted[0];
 };
 
 // Do not edit below this line
